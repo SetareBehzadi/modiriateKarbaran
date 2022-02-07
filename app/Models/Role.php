97b283ba0855c8bcc2ecf2434bb44074aae5212a
SetareBehzadi <setare.behzadi@gmail.com>
2022-02-07 17:49:03 +0330

    all about permissions

---
+++ app/Models/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use App\Services\Permission\Traits\HasPermissions;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+    use HasPermissions;
+
+    protected $fillable = ['name', 'persian_name'];
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::group(['prefix' => 'panel', 'middleware' => 'role:admin' , 'namespace'=>'App\Http\Controllers'], function () {
+    Route::get('users', 'UserController@index')->name('users.index');
+    Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit');
+    Route::post('users/{user}/edit', 'UserController@update')->name('users.update');
+    Route::get('roles', 'RoleController@index')->name('roles.index');
+    Route::post('roles', 'RoleController@store')->name('roles.store');
+    Route::get('roles/{role}/edit', 'RoleController@edit')->name('roles.edit');
+    Route::post('roles/{role}/edit', 'RoleController@update')->name('roles.update');
+});
